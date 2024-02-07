2d5f78f012a09fa52c1ad162e2dc8e6874a16bde
Reginaldo Evaristo <72899408+oEvaristo@users.noreply.github.com>
2024-02-06 22:46:03 -0300

    movimentando

--- Assets/Jogador/Movimentacao.cs
+++ Assets/Jogador/Movimentacao.cs
@@ -5,74 +5,34 @@ using UnityEngine;
 public class Movimentacao : MonoBehaviour
 {
     private CharacterController controle;
-    private Animator animacao;
-    private bool estaNoChao = true;
-    private Vector3 movimentacao;
-    private Vector3 direcaoPulo;
     private float horizontal;
-
-    public float velocidadeMovimento = 10f;
-    public float gravidade = 9.8f;
-    public float forcaPulo = 2f;
-    public float velocidadeRotacao = 5f;
-
+    private Vector3 movimentacao;
+    
+    public float velocidadeMovimento;
+    public float velocidadeRotacao;
+    
     void Start()
     {
         controle = GetComponent<CharacterController>();
-        animacao = GetComponent<Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
         Movimento();
         Rotacao();
-        Pulo();
     }
 
-    private void Movimento()
-    {
+    private void Movimento(){
         horizontal = Input.GetAxis("Horizontal");
-        estaNoChao = controle.isGrounded;
         movimentacao = new Vector3(horizontal * velocidadeMovimento, 0, 0);
         controle.Move(movimentacao * Time.deltaTime);
-
-        // Gravidade
-        direcaoPulo.y -= gravidade * Time.deltaTime;
     }
 
-    private void Rotacao()
-    {
-        if (movimentacao != Vector3.zero)
-        {
-            animacao.SetBool("Andando", true);
-            Quaternion novaRotation = Quaternion.LookRotation(movimentacao);
-            transform.rotation = Quaternion.Slerp(transform.rotation, novaRotation, velocidadeRotacao * Time.deltaTime);
+    private void Rotacao(){
+        if(movimentacao != Vector3.zero){
+            Quaternion novaRotacao = Quaternion.LookRotation(movimentacao);
+            transform.rotation = Quaternion.Slerp(transform.rotation, novaRotacao, velocidadeRotacao * Time.deltaTime);
         }
-        else
-        {
-            animacao.SetBool("Andando", false);
-        }
-    }
-
-    private void Pulo()
-    {
-
-        if (estaNoChao)
-        {
-            direcaoPulo.y = -0.5f;
-
-            if (Input.GetButtonDown("Jump"))
-            {
-                direcaoPulo.y = forcaPulo;
-            }
-
-            animacao.SetBool("Pulando", false);
-        }
-        else
-        {
-            animacao.SetBool("Pulando", true);
-        }
-
-        controle.Move(direcaoPulo * Time.deltaTime);
     }
 }
